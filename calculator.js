4e8668225af3b4188cdde155b858e12b04dfb6b1
Kshitija Sanjiv Savairam <98964354+SavairamK@users.noreply.github.com>
2023-12-30 14:03:57 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,30 @@
+function add(){
+    num1 = parseInt(window.prompt("Enter first number"))
+    num2 = parseInt(window.prompt("Enter second number"))
+    addition = num1 + num2
+    alert(addition)
+}
+function sub(){
+    num1 = parseInt(window.prompt("Enter first number"))
+    num2 = parseInt(window.prompt("Enter second number"))
+    subtraction = num1 - num2
+    alert(subtraction)
+}
+function mul(){
+    num1 = parseInt(window.prompt("Enter first number"))
+    num2 = parseInt(window.prompt("Enter second number"))
+    multiplication = num1 * num2
+    alert(multiplication)
+}
+function div(){
+    num1 = parseInt(window.prompt("Enter first number"))
+    num2 = parseInt(window.prompt("Enter second number"))
+    division = num1 / num2
+    alert(division)
+}
+function mod(){
+    num1 = parseInt(window.prompt("Enter first number"))
+    num2 = parseInt(window.prompt("Enter second number"))
+    modulus = num1 % num2
+    alert(modulus)
+}
